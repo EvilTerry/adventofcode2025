0f2dae56cd8bca2ff2621bf234a357df3ae2ea2e
Egidijus <evilterixyoutub@gmail.com>
2025-12-02 00:11:52 +0100

    Solve 1-2

--- 1/main.py
+++ 1/main.py
@@ -1,3 +1,5 @@
+import math
+
 def task1(rotations: list[str]) -> int:
     dial = 50
     password = 0
@@ -16,6 +18,29 @@ def task1(rotations: list[str]) -> int:
 
     return password
 
+def task2(rotations: list[str]) -> int:
+    dial = 50
+    password = 0
+
+    for r in rotations:
+        rotation_direction = r[0] == 'R'
+        rotation_count = int(r[1:])
+
+        for _ in range(rotation_count):
+            if rotation_direction:
+                dial += 1
+            else:
+                dial -= 1
+
+            if dial == 100:
+                dial = 0
+            elif dial == -1:
+                dial = 99
+            
+            if dial == 0:
+                password += 1
+
+    return password
 
 if __name__ == '__main__':
     file = open('1/input.txt')
@@ -23,3 +48,4 @@ if __name__ == '__main__':
     file.close()
 
     print(task1(rotations))
+    print(task2(rotations))
