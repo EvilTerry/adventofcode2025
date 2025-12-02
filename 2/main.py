953c114fbc67bd1a5fff1855b753baed02f371ba
Egidijus <evilterixyoutub@gmail.com>
2025-12-02 19:24:59 +0100

    Solve 2-2

--- 2/main.py
+++ 2/main.py
@@ -16,7 +16,24 @@ def task1(ids: list[str]) -> str:
     return output
 
 def task2(ids: list[str]) -> str:
-    pass
+    output = 0
+
+    for id in ids:
+        id_range = id.split('-')
+        for i in range(int(id_range[0]), int(id_range[1]) + 1):
+            str_i = str(i)
+
+            n = len(str_i) / 2
+
+            for k in range(1, int(n) + 1):
+                if len(str_i) % k == 0:
+                    parts = [str_i[i:i+k] for i in range(0, len(str_i), k)]
+                    
+                    if len(set(parts)) == 1:
+                        output += int(str_i)
+                        break
+
+    return output
 
 if __name__ == '__main__':
     file = open('2/input.txt')
@@ -24,3 +41,4 @@ if __name__ == '__main__':
     file.close()
 
     print(task1(ids))
+    print(task2(ids))
