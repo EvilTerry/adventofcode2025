36c46a55fe0158128e79012fbfabb80a000d4814
Egidijus <evilterixyoutub@gmail.com>
2025-12-03 21:28:09 +0100

    Solve 3-2

--- 3/main.py
+++ 3/main.py
@@ -4,7 +4,7 @@ def task1(banks: list[str]) -> str:
     for bank in banks:
         pointer_1 = 0
         pointer_2 = 0
-        
+
         for i in range(len(bank)):
             joltage = int(bank[i])
 
@@ -21,7 +21,28 @@ def task1(banks: list[str]) -> str:
 
 
 def task2(banks: list[str]) -> str:
-    ...
+    total_joltage = 0
+
+    for bank in banks:
+        # Convert string into list of chars
+        bank = list(bank)
+        joltage = ''
+
+        for i in range(12):
+            # Define the searching range
+            search_array = bank[:len(bank) - (11 - i)]
+
+            # Find highest number from the search range
+            highest_number = sorted(search_array, reverse=True)[0]
+
+            # Remove joltages until the highest number
+            bank = bank[bank.index(highest_number) + 1:]
+
+            joltage += highest_number
+
+        total_joltage += int(joltage)
+
+    return total_joltage
 
 if __name__ == '__main__':
     file = open('3/input.txt')
@@ -29,3 +50,4 @@ if __name__ == '__main__':
     file.close()
 
     print(task1(banks))
+    print(task2(banks))
