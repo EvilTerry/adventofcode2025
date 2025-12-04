dbbb1d5db5d9a353493949e1cbd8eab493af1f20
Egidijus <evilterixyoutub@gmail.com>
2025-12-04 15:24:36 +0100

    Solve 4-1

---
+++ 4/main.py
@@ -0,0 +1,57 @@
+def task1(rolls: list[list[chr]]) -> int:
+    accesible_rolls = 0
+
+    for y in range(len(rolls)):
+
+        for x in range(len(rolls[y])):
+            if rolls[y][x] != '@':
+                continue
+
+            adjacent_rolls = 0
+            
+            # Check top 3
+            if y > 0 and x > 0 and rolls[y - 1][x - 1] == '@':
+                adjacent_rolls += 1
+
+            if y > 0 and rolls[y - 1][x] == '@':
+                adjacent_rolls += 1
+
+            if y > 0 and x < len(rolls[y]) - 1 and rolls[y - 1][x + 1] == '@':
+                adjacent_rolls += 1
+
+            # Check left
+            if x > 0 and rolls[y][x - 1] == '@':
+                adjacent_rolls += 1
+
+            # Check right
+            if x < len(rolls[y]) - 1 and rolls[y][x + 1] == '@':
+                adjacent_rolls += 1
+
+            # Check bottom 3
+            if y < len(rolls) - 1 and x > 0 and rolls[y + 1][x - 1] == '@':
+                adjacent_rolls += 1
+
+            if y < len(rolls) - 1 and rolls[y + 1][x] == '@':
+                adjacent_rolls += 1
+
+            if y < len(rolls) - 1 and x < len(rolls[y]) - 1 and rolls[y + 1][x + 1] == '@':
+                adjacent_rolls += 1
+
+            if adjacent_rolls < 4:
+                accesible_rolls += 1
+
+    return accesible_rolls
+
+def task2(rolls: list[list[chr]]) -> int:
+    ...
+
+if __name__ == '__main__':
+    input_file_path = '4/input.txt'
+
+    with open(input_file_path) as file:
+        input = file.read().split('\n')
+
+    rolls = [list(line) for line in input]
+
+    print(task1(rolls))
+    # print(task2(rolls))
