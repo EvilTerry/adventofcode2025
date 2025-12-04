b64a313cfefeb291a86c29f0991bedce1d4a2ab3
Egidijus <evilterixyoutub@gmail.com>
2025-12-04 15:56:51 +0100

    Improve code 4

--- 4/main.py
+++ 4/main.py
@@ -1,107 +1,58 @@
-def task1(rolls: list[list[chr]]) -> int:
-    accesible_rolls = 0
+def check_roll(rolls: list[list[str]], x: int, y: int) -> bool:
+    if x < 0 or y < 0:
+        return False
+    try:
+        return rolls[y][x] == '@'
+    except IndexError:
+        return False
 
-    for y in range(len(rolls)):
+def count_adjacent(rolls: list[list[str]], x: int, y: int) -> int:
+    offsets = [(-1,-1), (0,-1), (1,-1), (-1,0), (1,0), (-1,1), (0,1), (1,1)]
+    return sum(check_roll(rolls, x+dx, y+dy) for dx, dy in offsets)
 
-        for x in range(len(rolls[y])):
-            if rolls[y][x] != '@':
+def task1(rolls: list[list[str]]) -> int:
+    accessible_rolls = 0
+
+    for y, row in enumerate(rolls):
+        for x, roll in enumerate(row):
+            if roll != '@':
                 continue
 
-            adjacent_rolls = 0
-            
-            # Check top 3
-            if y > 0 and x > 0 and rolls[y - 1][x - 1] == '@':
-                adjacent_rolls += 1
+            if count_adjacent(rolls, x, y) < 4:
+                accessible_rolls += 1
 
-            if y > 0 and rolls[y - 1][x] == '@':
-                adjacent_rolls += 1
+    return accessible_rolls
 
-            if y > 0 and x < len(rolls[y]) - 1 and rolls[y - 1][x + 1] == '@':
-                adjacent_rolls += 1
-
-            # Check left
-            if x > 0 and rolls[y][x - 1] == '@':
-                adjacent_rolls += 1
-
-            # Check right
-            if x < len(rolls[y]) - 1 and rolls[y][x + 1] == '@':
-                adjacent_rolls += 1
-
-            # Check bottom 3
-            if y < len(rolls) - 1 and x > 0 and rolls[y + 1][x - 1] == '@':
-                adjacent_rolls += 1
-
-            if y < len(rolls) - 1 and rolls[y + 1][x] == '@':
-                adjacent_rolls += 1
-
-            if y < len(rolls) - 1 and x < len(rolls[y]) - 1 and rolls[y + 1][x + 1] == '@':
-                adjacent_rolls += 1
-
-            if adjacent_rolls < 4:
-                accesible_rolls += 1
-
-    return accesible_rolls
-
-def task2(rolls: list[list[chr]]) -> int:
-    total_accesible_rolls = 0
+def task2(rolls: list[list[str]]) -> int:
+    total_accessible_rolls = 0
 
     while True:
-        ref_rolls = rolls.copy()
-        accesible_rolls = 0
+        ref_rolls = [row[:] for row in rolls]
+        accessible_rolls = 0
 
-        for y in range(len(ref_rolls)):
-            for x in range(len(ref_rolls[y])):
-                if ref_rolls[y][x] != '@':
+        for y, row in enumerate(ref_rolls):
+            for x, roll in enumerate(row):
+                if roll != '@':
                     continue
 
-                adjacent_rolls = 0
-                
-                # Check top 3
-                if y > 0 and x > 0 and ref_rolls[y - 1][x - 1] == '@':
-                    adjacent_rolls += 1
-
-                if y > 0 and ref_rolls[y - 1][x] == '@':
-                    adjacent_rolls += 1
-
-                if y > 0 and x < len(ref_rolls[y]) - 1 and ref_rolls[y - 1][x + 1] == '@':
-                    adjacent_rolls += 1
-
-                # Check left
-                if x > 0 and ref_rolls[y][x - 1] == '@':
-                    adjacent_rolls += 1
-
-                # Check right
-                if x < len(ref_rolls[y]) - 1 and ref_rolls[y][x + 1] == '@':
-                    adjacent_rolls += 1
-
-                # Check bottom 3
-                if y < len(ref_rolls) - 1 and x > 0 and ref_rolls[y + 1][x - 1] == '@':
-                    adjacent_rolls += 1
-
-                if y < len(ref_rolls) - 1 and ref_rolls[y + 1][x] == '@':
-                    adjacent_rolls += 1
-
-                if y < len(ref_rolls) - 1 and x < len(ref_rolls[y]) - 1 and ref_rolls[y + 1][x + 1] == '@':
-                    adjacent_rolls += 1
-
-                if adjacent_rolls < 4:
-                    accesible_rolls += 1
+                if count_adjacent(ref_rolls, x, y) < 4:
+                    accessible_rolls += 1
                     rolls[y][x] = '.'
 
-        if accesible_rolls > 0:
-            total_accesible_rolls += accesible_rolls
-        else:
+        if accessible_rolls == 0:
             break
 
-    return total_accesible_rolls
+        total_accessible_rolls += accessible_rolls
+
+    return total_accessible_rolls
 
 if __name__ == '__main__':
     input_file_path = '4/input.txt'
 
     with open(input_file_path) as file:
-        input = file.read().split('\n')
+        lines = file.read().split('\n')
 
-    rolls = [list(line) for line in input]
+    rolls = [list(line) for line in lines]
 
     print(task1(rolls))
     print(task2(rolls))
