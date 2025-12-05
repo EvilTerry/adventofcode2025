5b50ce07d5d69c211e78fb23a707327c4f14a5c9
Egidijus <evilterixyoutub@gmail.com>
2025-12-05 16:06:09 +0100

    Solve 5-2

--- 5/main.py
+++ 5/main.py
@@ -12,8 +12,23 @@ def task1(fresh_ids: list[str], available_ids: list[str]) -> int:
 
     return total_count
 
-def task2(fresh_ids: list[str], available_ids: list[str]) -> int:
-    ...
+def task2(fresh_ids: list[str]) -> int:
+    fresh_ids = [[int(fresh_id.split('-')[0]), int(fresh_id.split('-')[1])] for fresh_id in fresh_ids]
+
+    fresh_ids.sort(key = lambda x: x[0])
+    merged = []
+    start, end = fresh_ids[0]
+
+    for s, e in fresh_ids[1:]:
+        if s <= end:  
+            end = max(end, e)
+        else:
+            merged.append((start, end))
+            start, end = s, e
+
+    merged.append((start, end))
+
+    return sum((start - end + 1) for start, end in merged)
 
 if __name__ == '__main__':
     input_file_path = '5/input.txt'
@@ -24,4 +39,5 @@ if __name__ == '__main__':
     fresh_ids = lines[:lines.index('')]
     available_ids = lines[lines.index('') + 1:]
 
-    print(task1(fresh_ids, available_ids))
+    # print(task1(fresh_ids, available_ids))
+    print(task2(fresh_ids))
